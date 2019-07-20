9249ecf0d1228d705fa3496f222632c0e11de956
himanoa <matsunoappy@gmail.com>
2019-07-21 04:55:10 +0900

    Fix

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { render } from "react-dom";
+import * as ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 if (root) {
-  render(<h1>hello</h1>, root);
+  ReactDOM.render(<h1>hello</h1>, root);
 }
