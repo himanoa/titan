ea504725e6bfe4896bf172efb66ce062a0821a98
himanoa <matsunoappy@gmail.com>
2019-07-21 03:21:51 +0900

    Fix webpack config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,15 +1,22 @@
 const webpack = require("webpack");
 const path = require("path");
+const { dependencies } = require("./package.json");
 
+const extensions = [".ts", ".tsx", ".json", ".js", ".jsx"];
 const createConfig = (mode, mainPlugin, rendererPlugin = []) => {
   return [
     {
+      externals: [...Object.keys(dependencies || {})],
+      resolve: {
+        extensions
+      },
       target: "electron-main",
       entry: { main: path.join(__dirname, "src", "main", "index.ts") },
       plugins: mainPlugin,
       output: {
         path: path.join(__dirname, "dist"),
-        filename: "main.js"
+        filename: "main.js",
+        libraryTarget: "commonjs2"
       },
       module: {
         rules: [
@@ -22,12 +29,15 @@ const createConfig = (mode, mainPlugin, rendererPlugin = []) => {
       }
     },
     {
+      externals: [...Object.keys(dependencies || {})],
       target: "electron-renderer",
       entry: "./src/renderer/index.tsx",
       plugins: rendererPlugin,
+      resolve: { extensions },
       output: {
         path: path.join(__dirname, "dist"),
-        filename: "renderer.js"
+        filename: "renderer.js",
+        libraryTarget: "commonjs2"
       },
       module: {
         rules: [
