7dddd368698a6b398990a8c913c5315bc6121cc2
himanoa <matsunoappy@gmail.com>
2019-08-21 00:59:16 +0900

    Remove main process configuration in webpack.config.base.js

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -7,29 +7,6 @@ const extensions = [".ts", ".tsx", ".json", ".js", ".jsx"];
 const createConfig = (mode, mainPlugin, rendererPlugin = []) => {
   return [
     {
-      externals: [...Object.keys(dependencies || {})],
-      resolve: {
-        extensions
-      },
-      target: "electron-main",
-      entry: { main: path.join(__dirname, "src", "main", "index.ts") },
-      plugins: mainPlugin,
-      output: {
-        path: path.join(__dirname, "dist"),
-        filename: "main.js",
-        libraryTarget: "commonjs2"
-      },
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-          }
-        ]
-      }
-    },
-    {
       // externals: ["electron"],
       target: "electron-renderer",
       entry: "./src/renderer/index.tsx",
